55b0b5fb1e879b60259c66789f534c284466a246
Michael McLoughlin <mmcloughlin@gmail.com>
2017-05-07 17:36:32 -0700

    add cmd flags

--- cmd/adorn/cmd/generate.go
+++ cmd/adorn/cmd/generate.go
@@ -7,26 +7,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var cfg adorn.Config
+
+func init() {
+	generateCmd.Flags().StringVarP(&cfg.Package, "package", "p", "", "package name")
+	generateCmd.Flags().StringVarP(&cfg.TypeName, "type", "t", "", "type name")
+	generateCmd.Flags().StringVarP(&cfg.MethodName, "method", "m", "", "method name")
+	generateCmd.Flags().StringSliceVarP(&cfg.ArgumentTypes, "args", "a", nil, "argument types")
+	generateCmd.Flags().StringSliceVarP(&cfg.ReturnTypes, "return", "r", nil, "return types")
+
+	RootCmd.AddCommand(generateCmd)
+}
+
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate type and adornments",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return generate()
+		return adorn.Generate(cfg, os.Stdout)
 	},
-}
-
-func init() {
-	RootCmd.AddCommand(generateCmd)
-}
-
-func generate() error {
-	cfg := adorn.Config{
-		Package:       "manners",
-		TypeName:      "Greeter",
-		MethodName:    "Greeting",
-		ArgumentTypes: []string{"string", "string"},
-		ReturnTypes:   []string{"string"},
-	}
-	return adorn.Generate(cfg, os.Stdout)
 }
