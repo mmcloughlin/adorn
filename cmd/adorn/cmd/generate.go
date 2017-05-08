dee79c27fad743a512d531ddde89459602b24071
Michael McLoughlin <mmcloughlin@gmail.com>
2017-05-07 17:48:10 -0700

    --output flag

--- cmd/adorn/cmd/generate.go
+++ cmd/adorn/cmd/generate.go
@@ -1,13 +1,17 @@
 package cmd
 
 import (
+	"io"
 	"os"
 
 	"github.com/mmcloughlin/adorn"
 	"github.com/spf13/cobra"
 )
 
-var cfg adorn.Config
+var (
+	cfg    adorn.Config
+	output string
+)
 
 func init() {
 	generateCmd.Flags().StringVarP(&cfg.Package, "package", "p", "", "package name")
@@ -15,6 +19,7 @@ func init() {
 	generateCmd.Flags().StringVarP(&cfg.MethodName, "method", "m", "", "method name")
 	generateCmd.Flags().StringSliceVarP(&cfg.ArgumentTypes, "args", "a", nil, "argument types")
 	generateCmd.Flags().StringSliceVarP(&cfg.ReturnTypes, "return", "r", nil, "return types")
+	generateCmd.Flags().StringVarP(&output, "output", "o", "", "output filename (defaults to stdout)")
 
 	RootCmd.AddCommand(generateCmd)
 }
@@ -24,6 +29,15 @@ var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate type and adornments",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return adorn.Generate(cfg, os.Stdout)
+		var w io.Writer = os.Stdout
+		if output != "" {
+			f, err := os.Create(output)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			w = f
+		}
+		return adorn.Generate(cfg, w)
 	},
 }
